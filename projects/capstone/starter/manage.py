c8a48f29fe28a02f856ef40e725469c8ea1eae36
jeremiahidoko <jeremiahidoko@gmail.com>
2020-07-14 21:00:57 +0100

    tired

--- projects/capstone/starter/manage.py
+++ projects/capstone/starter/manage.py
@@ -4,9 +4,9 @@ from flask_migrate import Migrate, MigrateCommand
 from app import APP 
 from models import db 
 
-migragte = Migrate(APP, db)
-manager = Manager(APP)
+migrate = Migrate(APP, db)
 
+manager = Manager(APP)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN='joi.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='Casting'
-export DATABASE_URL=postgres://dowmxfvstdlpuk:b977f38a414029251dd6600325d3ccac5110ff2894f8b3c437b46e2c135d5e47@ec2-107-20-15-85.compute-1.amazonaws.com:5432/d8na0vr3bbdvo0
+export DATABASE_URL='postgres://postgres@localhost:5432/casting'
